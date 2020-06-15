fe1afe263330fe9c7545f735e0af5a12daa0c9a9
dafaqSTEVEN <36439494+dafaqSTEVEN@users.noreply.github.com>
2020-06-15 19:00:54 +0800

    0.0.0
    chess board display [OK]

---
+++ chess.py
@@ -0,0 +1,54 @@
+from chess_class import *
+# for key,values in gp.items():
+#     print(key,values.name,values.side)
+
+# print_chess()
+# # print(default)
+# turn = 1
+# while True:
+#     if turn % 2 == 1:
+#         inp = input('[White] Select.\n')
+#         if gp[inp].type == 'o':
+#             print('Nothing is selected')
+#         elif gp[inp].side == 'white':
+#             print(str(gp[inp].side),str(gp[inp].dp),'at',str(inp),'has been selected.')
+#             fin = input('Destination?\n')
+#             if gp[fin].type == 'o':
+#                 gp[fin] =  chess(gp[inp].side,gp[inp].id,gp[inp].type,gp[inp].dp)
+#                 gp[inp] = chess('na', 0, 'o', 'blank')
+#                 print('Done.')
+#                 turn +=1
+#             elif gp[inp].side != gp[fin].side:
+#                 print(str(gp[fin].side),str(gp[fin].dp),'at',str(fin),'has been taken.')
+#                 gp[fin] = chess(gp[inp].side, gp[inp].id, gp[inp].type, gp[inp].dp)
+#                 gp[inp] = chess('na', 0, 'o', 'blank')
+#                 turn +=1
+#             else:
+#                 print('You cant take your own chess.')
+#         else:
+#             print('Its not your turn.')
+#     elif turn % 2 == 0:
+#         inp = input('[Black] Select.\n')
+#         if gp[inp].type == 'o':
+#             print('Nothing is selected')
+#         elif gp[inp].side == 'black':
+#             print(str(gp[inp].side),str(gp[inp].dp),'at',str(inp),'has been selected.')
+#             fin = input('Destination?\n')
+#             if gp[fin].type == 'o':
+#                 gp[fin] =  chess(gp[inp].side,gp[inp].id,gp[inp].type,gp[inp].dp)
+#                 gp[inp] = chess('na', 0, 'o', 'blank')
+#                 print('Done.')
+#                 turn +=1
+#             elif gp[inp].side != gp[fin].side:
+#                 print(str(gp[fin].side),str(gp[fin].dp),'at',str(fin),'has been taken.')
+#                 gp[fin] = chess(gp[inp].side, gp[inp].id, gp[inp].type, gp[inp].dp)
+#                 gp[inp] = chess('na', 0, 'o', 'blank')
+#                 turn +=1
+#             else:
+#                 print('You cant take your own chess.')
+#         else:
+#             print('Its not your turn.')
+#
+#     print_chess()
+
+print_gp()
---
+++ chess_class.py
@@ -0,0 +1,139 @@
+class chess():
+    def __init__(self,side,id,type,dp):
+        self.type = 'chess'
+        self.side = side
+        self.type = type
+        self.id = id
+        self.dp = dp
+
+# def find(self):
+#     return(gp[self].type)
+#
+# # def adv_type(self):
+# #     return(gp[self].type)
+#
+# gp = {}
+#
+# w_pawn_location = ['a2','b2','c2','d2','e2','f2','g2','h2']
+# w_rook_location = ['a1','h1']
+# w_knight_location = ['b1','g1']
+# w_bishop_location = ['c1','f1']
+# w_queen_location = ['d1']
+# w_king_location = ['e1']
+#
+# for i in range(len(w_pawn_location)):
+#     gp[w_pawn_location[i]] = chess('white',i,'p','pawn')
+#
+#
+# for i in range(len(w_rook_location)):
+#     gp[w_rook_location[i]] = chess('white',i,'r','rook')
+#
+# for i in range(len(w_knight_location)):
+#     gp[w_knight_location[i]] = chess('white',i,'k','knight')
+#
+# for i in range(len(w_bishop_location)):
+#     gp[w_bishop_location[i]] = chess('white',i,'b','bishop')
+#
+# gp[w_queen_location[0]] = chess('white',0,'q','queen')
+# gp[w_king_location[0]] = chess('white',0,'k','king')
+#
+# # *********************************************
+#
+# b_pawn_location = ['a7','b7','c7','d7','e7','f7','g7','h7']
+# b_rook_location = ['a8','h8']
+# b_knight_location = ['b8','g8']
+# b_bishop_location = ['c8','f8']
+# b_queen_location = ['d8']
+# b_king_location = ['e8']
+#
+# for i in range(len(w_pawn_location)):
+#     gp[b_pawn_location[i]] = chess('black',i,'P','pawn')
+#
+#
+# for i in range(len(w_rook_location)):
+#     gp[b_rook_location[i]] = chess('black',i,'R','rook')
+#
+# for i in range(len(w_knight_location)):
+#     gp[b_knight_location[i]] = chess('black',i,'K','knight')
+#
+# for i in range(len(w_bishop_location)):
+#     gp[b_bishop_location[i]] = chess('black',i,'B','bishop')
+#
+# gp[b_queen_location[0]] = chess('black',0,'Q','queen')
+# gp[b_king_location[0]] = chess('black',0,'K','king')
+#
+# i = 3
+# list = ['a','b','c','d','e','f','g','h']
+# while i < 7:
+#     for y in range(len(list)):
+#         gp[str(list[y]) + str(i)] = chess('na',0,'o','blank')
+#     i += 1
+#
+#
+# def print_chess():
+#     print('a-b-c-d-e-f-g-h-~')
+#     i = 8
+#     while i > 0:
+#         print(find('a' + str(i)), find('b' + str(i)), find('c' + str(i)), find('d' + str(i)), find('e' + str(i)),
+#               find('f' + str(i)), find('g' + str(i)), find('h' + str(i)),str(i))
+#         i -= 1
+#
+#     print('----------------')
+#         # print('r k b q k b k r\n'
+#         #       'p p p p p p p p\n'
+#         #       'o o o o o o o o\n'
+#         #       'o o o o o o o o\n'
+#         #       'o o o o o o o o\n'
+#         #       'o o o o o o o o\n'
+#         #       'p p p p p p p p\n'
+#         #       'r k b q k b k r')
+#
+# default = []
+# for x in range(8):
+#     for i in range(len(list)):
+#         default.append(str(list[i])+str(x+1))
+#
+#
+
+a = []
+b = []
+c = []
+d = []
+e = []
+f = []
+g = []
+h = []
+
+gp = [a,b,c,d,e,f,g,h]
+for i in range(8):
+    while len(gp[i]) < 8:
+        gp[i].append(chess('na',0,'blank','o'))
+
+for i in range(8):
+    gp[i][6] = chess('black',i,'pawn','P')
+    gp[i][1] = chess('white',i,'pawn','p')
+    if i == 0 or i == 7:
+        gp[i][0] = chess('white',i,'rook','r')
+        gp[i][7] = chess('black',i,'rook','R')
+    if i == 1 or i == 6:
+        gp[i][0] = chess('white',i,'knight','k')
+        gp[i][7] = chess('black',i,'knight','K')
+    if i == 2 or i == 5:
+        gp[i][0] = chess('white',i,'bishop','b')
+        gp[i][7] = chess('black', i, 'bishop', 'B')
+    if i == 3:
+        gp[i][0] = chess('white', i, 'queen', 'q')
+        gp[i][7] = chess('black', i, 'queen', 'Q')
+    if i == 4:
+        gp[i][0] = chess('white', i, 'king', 'k')
+        gp[i][7] = chess('black', i, 'king', 'K')
+
+
+def print_gp():
+    i = 7
+    while i>=0:
+        print(a[i].dp,b[i].dp,c[i].dp,d[i].dp,e[i].dp,f[i].dp,g[i].dp,h[i].dp,'\n')
+        i-=1
+
+
+
